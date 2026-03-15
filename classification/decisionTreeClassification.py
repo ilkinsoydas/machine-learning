518d2ed3bcfc87de5f5e350c35c75842ffb83d13
“ilkinsoydas” <ilkinsoydas123@gmail.com>
2026-03-15 17:00:48 +0300

    DecisionTreeClassifier algoritmasını öğrendim

---
+++ classification/decisionTreeClassification.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('../Heart Attack Data Set.csv')
+print(df.head(3))
+
+y = df['target']
+x = df.drop("target", axis = 1)
+
+tree = DecisionTreeClassifier()
+model = tree.fit(x,y)
+
+print(model.score(x, y))
+# train test split ile
+
+x_train,x_test, y_train, y_test = train_test_split(x,y, random_state=16, train_size=0.7 )
+
+tree = DecisionTreeClassifier()
+model = tree.fit(x_train, y_train)
+print(model.score(x_test, y_test))
+
+#train test split yokkken 1 çıkmıştı, kullandıktan sonra 0.80.. çıktı, yani model aşırı öğrenmeye maruz kalmış çıkarımını yapabiliriz
+
+denemex = [[31,1,2,130,240,0,0,150,0,2,0,0,2]]
+deneme = pd.DataFrame(denemex, columns=x.columns)
+prediction = model.predict(deneme)
+print(prediction)
+
+
+
